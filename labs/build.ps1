8ae4629594b4b57da029c7cf94d548c2eb861df5
Ryan Tolboom <rxt1077@njit.edu>
2022-02-07 16:38:45 -0500

    Added log4j lab

--- labs/build.ps1
+++ labs/build.ps1
@@ -1,2 +1,3 @@
-Compress-Archive -Force -Path nmap/* -DestinationPath nmap.zip
-Compress-Archive -Force -Path scapy/* -DestinationPath scapy.zip
+Compress-Archive -Force -Path nmap/ -DestinationPath nmap.zip
+Compress-Archive -Force -Path scapy/ -DestinationPath scapy.zip
+Compress-Archive -Force -Path log4j/ -DestinationPath log4j.zip
